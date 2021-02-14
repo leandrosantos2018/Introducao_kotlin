5acb43d647f67526af170e3e15899f9432e3a6e4
leandro <leleuramos@gmail.com>
2021-02-14 14:50:08 -0300

    teste

--- digitalinnoventionone/src/main/kotlin/one/digitalinnovention/digitalbank/Pessoa.kt
+++ digitalinnoventionone/src/main/kotlin/one/digitalinnovention/digitalbank/Pessoa.kt
@@ -1,4 +1,12 @@
 package one.digitalinnovention.digitalbank
 
 class Pessoa {
+    var nome: String ="Leandro"
+    var cpf:String ="145.255.164-78"
+}
+
+fun main() {
+    val Leandro = Pessoa()
+    println(Leandro.nome)
+    println(Leandro.cpf)
 }
