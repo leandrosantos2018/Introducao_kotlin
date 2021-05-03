cbff72b5e9f4da6109a7b9d8e618ed6d3b5c3968
leandro <leleuramos@gmail.com>
2021-05-03 14:55:53 -0300

    doublearrayof

--- api-collentions/src/main/kotlin/one/digitalinnovention/collections/TesteintArray.kt
+++ api-collentions/src/main/kotlin/one/digitalinnovention/collections/TesteintArray.kt
@@ -1,4 +1,32 @@
 package one.digitalinnovention.collections
 
-class TesteintArray {
+fun main(){
+
+    val values = IntArray(5)
+
+    values[0] = 1
+    values[1] = 7
+    values[2] = 6
+    values[3] = 3
+    values[4] = 2
+
+    print("----interação com for---\n")
+    for(valor in values){
+        println(valor)
+    }
+        print("------Segunda interação Percorrendo ARRAY-------\n")
+    values.forEach { valor ->
+        println(valor)
+    }
+    print("------interação com index-----\n")
+    for (index in values.indices){
+        println(values[index])
+    }
+
+    values.sort()
+
+    print("------interação  prinit ordenado-----\n")
+    for (valor in values){
+        println(valor)
+    }
 }
--- api-collentions/src/main/kotlin/one/digitalinnovention/collections/TesteintArrayOf.kt
+++ api-collentions/src/main/kotlin/one/digitalinnovention/collections/TesteintArrayOf.kt
@@ -1,4 +1,16 @@
 package one.digitalinnovention.collections
 
-class TesteintArrayOf {
+fun main() {
+    val values = intArrayOf(2,3,4,1,10,7);
+
+    values.forEach {
+        println(it)
+    }
+
+    values.sort()
+println("---- imprimi orderna-----")
+    values.forEach {
+        println(it)
+    }
+
 }
