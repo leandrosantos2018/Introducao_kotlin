a77d737924ab4b9e420c0487ac01e5519385a239
leandrosantos2018 <40071307+leandrosantos2018@users.noreply.github.com>
2022-08-18 21:28:34 -0300

    Update clientes.java

--- bank/src/bank/clientes.java
+++ bank/src/bank/clientes.java
@@ -3,7 +3,7 @@ package bank;
 public class clientes {
     public static void main(String[] args) {
         final String nome = "Leandro";
-        final String CPF = "145416427-19";
+        final String CPF = "xxxxxxxxxxx";
         bank Bank = new bank();
 
         System.out.println("o cliente " + nome + " que tem o CPF "+ CPF + " e tem segue conta e agencia " + "Conta  "+ Bank.getConta() + " Agencia  "+ Bank.getAgencia());
